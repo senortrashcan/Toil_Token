49121edfed3943ce88b419c92564df831456ec49
senortrashcan <hnl9324@gmail.com>
2023-10-12 15:09:35 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => (
                   className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
                     index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
                   }`}
-                  onClick={() => window.open(footerlink.links[link])}
+                  onClick={() => window.open(link.link)}
                 >
                   {link.name}
                 </li>
